be94e6dbd3c4e7871657025605157f307493aa59
vinid14 <vinidkumarpagolu@gmail.com>
2020-07-26 03:23:43 +0530

    helloworld

--- src/App.js
+++ src/App.js
@@ -8,15 +8,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+Hello world        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React s
+          Learn React 
         </a>
       </header>
     </div>
