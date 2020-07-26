4d6a91ba5d93e5e63478c41fc366bf339bb76475
vinid14 <vinidkumarpagolu@gmail.com>
2020-07-26 20:20:43 +0530

    arraowFunction

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,9 @@
 import React from  'react'
 
-function Greet(){
-    return <h1>Hello vinid</h1>
-}
+// function Greet(){
+//     return <h1>Hello vinid</h1>
+// }
+
+const Greet =() => <h1>Hello VinidKumar</h1>
 
 export default Greet
