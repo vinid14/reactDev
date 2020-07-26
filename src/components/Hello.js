75bd4f841013d626cf0eda6f362eee5ead824edf
vinid14 <vinidkumarpagolu@gmail.com>
2020-07-26 22:43:09 +0530

    reactJSXcreateElement

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const Hello =() =>{
-     return(
-         <div>
-             <h1>Hello Vinidkumar Pagolu</h1>
-         </div>
-     )
+    //  return(
+    //      <div>
+    //          <h1>Hello Vinidkumar Pagolu</h1>
+    //      </div>
+    //  )
+    return React.createElement('div',
+    {id:'hello',className:'dummyclass'},
+    React.createElement('h1',null,'Hello VinidKumar Pagolu'))
 }
 
 export default Hello
