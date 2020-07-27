2fb4882989c2fed54e932a9a3116b7a767c76ae4
vinid14 <vinidkumarpagolu@gmail.com>
2020-07-28 01:39:03 +0530

    RendringListof arrayObjects

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import Person from './Person'
 
 function NameList() {
-    const names=['vinid','kumar','pagolu']
+    const person=[
+        {
+            name:'Vinid',
+            age:25,
+            skill:'Angular'
+        },
+        {
+            name:'Kumar',
+            age:28,
+            skill:'Java'
+        },
+        {
+            name:'Pagolu',
+            age:29,
+            skill:'React'
+        }
+    ]
+    const perstonList= person.map(person => <Person person={person}/>)
     return (
-        <div>
-            {
-              names.map( tempName => <h2>{tempName}</h2>)  
-            }
+        <div> {perstonList}
+    
 
         </div>
     )
