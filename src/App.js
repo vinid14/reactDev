1e56ce186de0894c572e397f893a90af63c4b73b
vinid14 <vinidkumarpagolu@gmail.com>
2020-07-27 23:19:07 +0530

    EventBinding-Approch1

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import Message from './components/message';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 
 function App() {
   return (
     <div className="App">
-      <FunctionClick/>
-      <ClassClick/>
+      <EventBind/>
+      {/* <FunctionClick/> */}
+      {/* <ClassClick/> */}
       {/* <Counter/> */}
       {/* <Message/> */}
 
---
+++ src/components/EventBind.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class EventBind extends Component {
+constructor(props) {
+    super(props)
+
+    this.state = {
+         message:'hello'
+    }
+}
+
+clickHandler(){
+    this.setState({
+        message:'good bye'
+    })
+}
+
+    render() {
+        return (
+            <div>
+                <div>{this.state.message}</div>
+               <button onClick={this.clickHandler.bind(this)}>click</button> 
+            </div>
+        )
+    }
+}
+
+export default EventBind
