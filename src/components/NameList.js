94d9ae9ccb327837ea260240e234d3da7be938f2
vinid14 <vinidkumarpagolu@gmail.com>
2020-07-28 01:26:41 +0530

    BasicListRendring

---
+++ src/components/NameList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NameList() {
+    const names=['vinid','kumar','pagolu']
+    return (
+        <div>
+            <h2>{names[0]}</h2>
+            <h2>{names[1]}</h2>
+            <h2>{names[2]}</h2>
+
+        </div>
+    )
+}
+
+export default NameList
