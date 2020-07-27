96c0db9b51a7adce37e52688f3afe60a62d24bf8
vinid14 <vinidkumarpagolu@gmail.com>
2020-07-28 01:14:41 +0530

    ConditionalRendering

---
+++ src/components/UserGreeting.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+ class UserGreeting extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            isLoggedIn:true 
+        }
+    }
+    
+    render() {
+        if(this.state.isLoggedIn){
+            return (
+                <div>
+                
+                <div> Welcome Vinid</div>
+                </div>
+            )
+        }else{
+            return (
+                <div>
+                
+                <div> Welcome Guest</div>
+                </div>
+            )
+        }
+       
+    }
+}
+
+export default UserGreeting
