1455b144298bcdef1673b222dc88ef471ecc7384
vinid14 <vinidkumarpagolu@gmail.com>
2020-07-28 01:08:32 +0530

    methodAsprop-with-parameter

--- src/components/ChildComponet.js
+++ src/components/ChildComponet.js
@@ -4,7 +4,7 @@ function ChildComponet(props) {
 
     return (
         <div>
-            <button onClick={props.greetHandler}> Greet Parent</button>
+            <button onClick={() => props.greetHandler('child')}> Greet Parent</button>
         </div>
     )
 }
