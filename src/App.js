d9fa0d4cd915a2c079e2134b41e23f3d2282e4ee
vinid14 <vinidkumarpagolu@gmail.com>
2020-07-29 01:15:44 +0530

    cssStyleModule

--- src/App.js
+++ src/App.js
@@ -14,11 +14,16 @@ import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
 import StyleSheet from './components/Stylesheet';
 import Inline from './components/Inline';
+import './appStyles.css';
+import styles from './appStyles.module.css';
 
 function App() {
   return (
     <div className="App">
-      <Inline/>
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.sucess}>Sucess</h1>
+
+      {/* <Inline/> */}
       {/* <StyleSheet primary ={true}/> */}
       {/* <NameList/> */}
       {/* <UserGreeting/> */}
