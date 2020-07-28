34ba303962dc7864a96f0806dbfd5b410a60cede
vinid14 <vinidkumarpagolu@gmail.com>
2020-07-29 00:56:31 +0530

    commit

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -4,22 +4,25 @@ import Person from './Person'
 function NameList() {
     const person=[
         {
+            id:1,
             name:'Vinid',
             age:25,
             skill:'Angular'
         },
         {
+            id:2,
             name:'Kumar',
             age:28,
             skill:'Java'
         },
         {
+            id:3,
             name:'Pagolu',
             age:29,
             skill:'React'
         }
     ]
-    const perstonList= person.map(person => <Person person={person}/>)
+    const perstonList= person.map(person => <Person key={person.id} person={person}/>)
     return (
         <div> {perstonList}
     
